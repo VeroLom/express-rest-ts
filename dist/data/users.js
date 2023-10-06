fe55018de3c0a55c4a3f79d9b25bb2e3063147cc
VeroLom <ver@0xff.su>
2023-10-06 18:25:37 +0200

    Added USERS data

---
+++ dist/data/users.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.usersData = void 0;
+exports.usersData = [
+    { id: 1, name: "Sören Wallace", login: "swallace", phone: "+1 99 9614321" },
+    { id: 1, name: "Lia Slováček", login: "lslovacek", phone: "+1 99 9154321" },
+    { id: 1, name: "Mirche Jäger", login: "mjager", phone: "+1 99 9651321" },
+    { id: 1, name: "Marcela Becskei", login: "mbecskei", phone: "+1 99 9654121" },
+    { id: 1, name: "Govinda Fiore", login: "gfiore", phone: "+1 99 9654311" },
+    { id: 1, name: "Dorota Paquet", login: "dpaquet", phone: "+1 99 9254321" },
+    { id: 1, name: "Walhberht Van der Aart", login: "wderaart", phone: "+1 99 9624321" },
+    { id: 1, name: "Adriano Jain", login: "ajain", phone: "+1 99 9654221" },
+    { id: 1, name: "Kossi Rose", login: "krose", phone: "+1 99 9354321" },
+    { id: 1, name: "Shapur Nováček", login: "snovacek", phone: "+1 99 9634321" },
+];
--- index.ts
+++ index.ts
@@ -1,9 +1,14 @@
 import express from "express";
+import {usersData} from "./data/users";
 
 const app = express();
 
 app.get("/", (req, res) => {
    res.send('Hallo von Express');
+});
+
+app.get("/users", (req, res) => {
+   res.json(usersData);
 });
 
 const port = process.env.PORT || 3333;
