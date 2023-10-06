10f4f1c60cfc854aa8bb72aec51be1901e60cba9
VeroLom <ver@0xff.su>
2023-10-06 21:49:30 +0200

    Fixed users ids

--- data/users.ts
+++ data/users.ts
@@ -7,13 +7,13 @@ interface IUser {
 
 export const usersData: IUser[] = [
     {id: 1, name: "Sören Wallace", login: "swallace", phone: "+1 99 9614321"},
-    {id: 1, name: "Lia Slováček", login: "lslovacek", phone: "+1 99 9154321"},
-    {id: 1, name: "Mirche Jäger", login: "mjager", phone: "+1 99 9651321"},
-    {id: 1, name: "Marcela Becskei", login: "mbecskei", phone: "+1 99 9654121"},
-    {id: 1, name: "Govinda Fiore", login: "gfiore", phone: "+1 99 9654311"},
-    {id: 1, name: "Dorota Paquet", login: "dpaquet", phone: "+1 99 9254321"},
-    {id: 1, name: "Walhberht Van der Aart", login: "wderaart", phone: "+1 99 9624321"},
-    {id: 1, name: "Adriano Jain", login: "ajain", phone: "+1 99 9654221"},
-    {id: 1, name: "Kossi Rose", login: "krose", phone: "+1 99 9354321"},
-    {id: 1, name: "Shapur Nováček", login: "snovacek", phone: "+1 99 9634321"},
+    {id: 2, name: "Lia Slováček", login: "lslovacek", phone: "+1 99 9154321"},
+    {id: 3, name: "Mirche Jäger", login: "mjager", phone: "+1 99 9651321"},
+    {id: 4, name: "Marcela Becskei", login: "mbecskei", phone: "+1 99 9654121"},
+    {id: 5, name: "Govinda Fiore", login: "gfiore", phone: "+1 99 9654311"},
+    {id: 6, name: "Dorota Paquet", login: "dpaquet", phone: "+1 99 9254321"},
+    {id: 7, name: "Walhberht Van der Aart", login: "wderaart", phone: "+1 99 9624321"},
+    {id: 8, name: "Adriano Jain", login: "ajain", phone: "+1 99 9654221"},
+    {id: 9, name: "Kossi Rose", login: "krose", phone: "+1 99 9354321"},
+    {id: 10, name: "Shapur Nováček", login: "snovacek", phone: "+1 99 9634321"},
 ];
