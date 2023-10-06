4c52b271fd1716fd03d8fff41cc3b4782c2465f6
VeroLom <ver@0xff.su>
2023-10-06 21:42:54 +0200

    Enable CORS

--- index.ts
+++ index.ts
@@ -2,12 +2,18 @@ import express from "express";
 import {usersData} from "./data/users";
 
 const app = express();
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   next();
+});
 
 app.get("/", (req, res) => {
    res.send('<a href="/users">users</a>');
 });
 
 app.get("/users", (req, res) => {
+   res.header("Access-Control-Allow-Origin", "*");
    res.json(usersData);
 });
 
