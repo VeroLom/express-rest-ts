12e1e888558412255f1abbac9a9256456c06b392
VeroLom <ver@0xff.su>
2023-10-19 15:06:27 +0200

    Added Bewerbungs db table, types and endpoint

--- index.ts
+++ index.ts
@@ -9,10 +9,33 @@ interface User {
    email: string;
    phone?: string;
 }
-interface DB {
-   users: User[];
+
+export enum BewerbungStatus {
+   STATUS_NEW = "STATUS_NEW",
+   STATUS_SENT = "STATUS_SENT",
+   STATUS_RESULT_POSITIVE = "STATUS_RESULT_POSITIVE",
+   STATUS_RESULT_NEGATIVE = "STATUS_RESULT_NEGATIVE",
+   STATUS_RESULT_POSITION_CLOSED = "STATUS_RESULT_POSITION_CLOSED",
 }
-const initial: DB = { users: [] };
+
+export interface Bewerbung {
+   id: number;
+   status: BewerbungStatus;
+   company: string;
+   city: string;
+   position: string;
+   sendDate?: string;
+   responseDate?: string;
+   note?: string;
+   link: string;
+}
+
+interface DB {
+   users?: User[];
+   bewerbungs?: Bewerbung[];
+}
+
+const initial: DB = { users: [], bewerbungs: [] };
 const db = new Database<DB>("./data/db.json", initial);
 /*db.update({
    users: []
@@ -28,12 +51,17 @@ app.use(function(req, res, next) {
 });
 
 app.get("/", (req, res) => {
-   res.send('<a href="/users">users</a>');
+   res.send('<a href="/users">users</a><a href="/bewerbungs">bewerbungs</a>');
 });
 
 app.get("/users", (req, res) => {
    res.header("Access-Control-Allow-Origin", "*");
    res.json(db.data.users);
+});
+
+app.get("/bewerbungs", (req, res) => {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.json(db.data.bewerbungs);
 });
 
 const port = process.env.PORT || 3333;
